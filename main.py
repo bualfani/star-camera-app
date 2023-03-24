e21f720a52a2696fcad7ee85ce14a0a33275850f
bahati <bulozealfani@gmail.com>
2023-03-24 10:00:36 -0500

    fixed the date and time

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ if start:
         check, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        dt = str(datetime.datetime.now())
+        # current time as datetime object
+        dt = datetime.now()
 
-        cv2.putText(img=frame, text=dt, org=(50, 50),
-                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255,64,64),
+        # Get the date
+        cv2.putText(img=frame, text=dt.strftime("%A"), org=(50, 50),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255,255,255),
+                    thickness=2, lineType=cv2.LINE_AA)
+        # Get the time
+        cv2.putText(img=frame, text=dt.strftime("%H:%M:%S"), org=(50, 50),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255, 64, 64),
                     thickness=2, lineType=cv2.LINE_AA)
 
         st_img.image(frame)
