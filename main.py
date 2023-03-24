1a5584ffa79a55833c6ce1fd2e2f2b64a043f789
bahati <bulozealfani@gmail.com>
2023-03-24 09:26:11 -0500

    added words on the camera with position and font size

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ if start:
     while True:
         check, frame = cv2.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        cv2.putText(img=frame, text="Camera Recording", org=(50, 50),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(20,100,200),
+                    thickness=2, lineType=cv2.LINE_AA)
